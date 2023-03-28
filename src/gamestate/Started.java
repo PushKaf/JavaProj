36214ce0236d3b127c88e5a31a5254c685c08af4
YetSquire <109554621+YetSquire@users.noreply.github.com>
2023-03-28 11:20:58 -0400

    Update Started.java

--- src/gamestate/Started.java
+++ src/gamestate/Started.java
@@ -10,16 +10,18 @@ import entities.*;
 import main.GamePanel;
 import utils.Constants.UI;
 import utils.DataLoader;
+import java.util.*;
 
 /**
  * 	State where the simulation has started
  */
-public class Started {
+public class Started<T extends Entity> {
 	
 	private BufferedImage backgroundImg;
 	private Dog dog;
 	private Tree tree;
 	private GamePanel gamePanel;
+	private ArrayList<T> ents;
 	
 	public Started(GamePanel gamePanel) {
 		
@@ -29,6 +31,8 @@ public class Started {
 		backgroundImg = DataLoader.GetSpriteData(DataLoader.MAIN_BACKGROUND);
 		dog = new Dog(0, 0);
 		tree = new Tree(200, 200);
+		
+		ents = new ArrayList<T>();
 	}
 	
 	
