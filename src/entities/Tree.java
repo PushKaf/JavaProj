791bc70f49bf1b0f77644cc4db1f113b157cecc7
Scrunkle <78704098+Scrunkley@users.noreply.github.com>
2023-04-21 12:49:00 -0400

    Add files via upload

--- src/entities/Tree.java
+++ src/entities/Tree.java
@@ -12,7 +12,7 @@ public class Tree extends Producer {
 	public static BufferedImage SPRITE = DataLoader.GetSpriteData(DataLoader.TREE_SPRITE);
 	public static int WIDTH = 128;
 	public static int HEIGHT = 64;
-	public static int ENERGY = 200;
+	public static int ENERGY = 0;
 	public static int REPRODUCEVAL = 300;
 	public static int GROWSPEED = 1;
 	public static int STRENGTH = 1;
